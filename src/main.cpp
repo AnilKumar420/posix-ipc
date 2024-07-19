74b6ff83c99f734e4c14d8046ca2f6666b579ec5
AnilKumar420 <105285391+AnilKumar420@users.noreply.github.com>
2024-07-19 12:09:39 +0200

    added a pointer

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,9 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
       std::cout << "Hello, World--------------!" << std::endl;
-    int x;
-    std::cout<<x<<std::endl;
+    int x=2;
+    int *ptr=&x
+    std::cout<<*ptr<<std::endl;
     return 0;
 }
 
