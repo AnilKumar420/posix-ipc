42fcc2d2d5a8df80c6a4121a57c7b4eb78d6331a
AnilKumar420 <anilkumar.ratnakaram@gmail.com>
2024-07-22 17:19:24 +0000

    Formatted code and fixed Clang-Tidy issues

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,18 @@
 #include <iostream>
 
 class Example {
-public:
-    void process()                     {
-        int* ptr = nullptr;
+  public:
+    void process()
+    {
+        int *ptr = nullptr;
         // Potential null pointer dereference
-        *ptr = 10;  // This line should trigger the null dereference check
-                                                                                }
+        *ptr = 10; // This line should trigger the null dereference check
+    }
 };
 
-int main() {
-                Example ex;
+int main()
+{
+    Example ex;
     ex.process();
     return 0;
 }
-
