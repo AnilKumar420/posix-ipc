94d367b2b5fe3357b3492e17b3f6662f6c55b52b
AnilKumar420 <105285391+AnilKumar420@users.noreply.github.com>
2024-07-22 12:25:23 +0200

    indetation error introduced

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,11 @@ public:
         int* ptr = nullptr;
         // Potential null pointer dereference
         *ptr = 10;  // This line should trigger the null dereference check
-    }
+                }
 };
 
 int main() {
-    Example ex;
+                Example ex;
     ex.process();
     return 0;
 }
