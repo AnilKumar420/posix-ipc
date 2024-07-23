fe9771d1f6135893ac25ae7839562826bdfd84e2
AnilKumar420 <anilkumar.ratnakaram@gmail.com>
2024-07-23 07:56:27 +0000

    Formatted code and fixed Clang-Tidy issues

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,9 @@
 #include <iostream>
 
 int main()
-        {
+{
     Calculator calc;
     int result = calc.add(2, 3);
     std::cout << "Result of addition: " << result << std::endl;
     return 0;
-                        }
+}
